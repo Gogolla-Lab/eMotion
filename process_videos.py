148d68873c8fcf5d5cd25ccce1b184d75246d829
onurserce <onurserce@gmail.com>
2020-07-26 17:14:08 +0200

    processVideo function changed to use pims pipelines. Added crop_frame function.

--- process_videos.py
+++ process_videos.py
@@ -5,9 +5,17 @@ import pandas as pd
 import imageio
 import pims
 from drawer import CircleDrawer
-from maskROIs import maskFrame_poly
+from maskROIs import maskFrame_circle
 from ast import literal_eval
 from joblib import Parallel, delayed
+
+
+def crop_frame(frame, crop):
+    """Generic function for cropping an image/frame."""
+    r = crop
+
+    return frame[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+
 
 def create_trims_df(folder):
 
@@ -30,7 +38,7 @@ def create_trims_df(folder):
     print(os.path.join(folder, 'trims.csv'), "created!")
 
 
-def append_mask_and_crop_ROIs(folder, shape = 'circle'):
+def append_mask_and_crop_ROIs(folder, shape='circle'):
 
     # Check if 'process.csv' exist to continue from where the user left the process
     if os.path.exists(os.path.join(folder, 'process.csv')):
@@ -65,49 +73,63 @@ def append_mask_and_crop_ROIs(folder, shape = 'circle'):
     print(os.path.join(folder, 'process.csv'), "created with all required entries!")
 
 
-def processVideo(videoPath, outputFolder):
-    """Pipeline containing function to trim, mask and crop a video (provided by the process.csv file)"""
+def processVideo(videoPath, outputFolder, shape='circle'):
+    """Pipeline containing function to trim, mask and crop a video (provided by the process.csv file)
+    :shape: 'circle' or 'polygon' """
 
     # get the required data from the process.csv file
     df = pd.read_csv(os.path.join(os.path.split(videoPath)[0], "process.csv"), index_col=0)
     start = df.loc[os.path.split(videoPath)[-1], 'start']
     end = df.loc[os.path.split(videoPath)[-1], 'end']
-    vertices = np.array(literal_eval(df.loc[os.path.split(videoPath)[-1], 'vertices']))
+    if shape == 'circle':
+        circle = literal_eval(df.loc[os.path.split(videoPath)[-1], 'circle'])
+    elif shape == 'polygon':
+        vertices = np.array(literal_eval(df.loc[os.path.split(videoPath)[-1], 'vertices']))
     crop = literal_eval(df.loc[os.path.split(videoPath)[-1], 'crop'])
     r = crop
 
-    # create the output folder
+    video = pims.ImageIOReader(videoPath)
+    fps = video.frame_rate
+
+    # pipelines
+    # masking pipeline
+    masking_pipeline = pims.pipeline(maskFrame_circle)
+    masking_kwargs = dict(circle=circle)
+    masked_video = masking_pipeline(video, **masking_kwargs)
+    # cropping pipeline
+    cropping_pipeline = pims.pipeline(crop_frame)
+    cropping_kwargs = dict(crop=crop)
+    cropped_video = cropping_pipeline(masked_video, **cropping_kwargs)
+
+    # writing to disk
     try:
         os.mkdir(outputFolder)
     except OSError:
         print(outputFolder, "already exists! Continuing with the process without creating it.")
 
-    # reader & writer
-    reader = imageio.get_reader(videoPath)
-    fps = reader.get_meta_data()['fps']
     outputFilename = os.path.join(outputFolder, os.path.split(videoPath)[-1].strip(".mp4") + "_processed.mp4")
-    writer = imageio.get_writer(outputFilename, fps=fps)
-
-    # writing to disk
-    start_frame = start * fps
-    end_frame = end * fps
-
-    for i, im in enumerate(reader):
-        if i >= start_frame and i <= end_frame:
-            masked = maskFrame_poly(im, vertices)
-            cropped = masked[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
-            writer.append_data(cropped)
-    writer.close()
+    start_frame = int(start * fps)
+    end_frame = int(end * fps)
+    imageio.mimwrite(outputFilename, cropped_video[start_frame:end_frame], fps=fps)
 
 
-def processVideos(folderPath, outputFolder, n_jobs=16):
+def processVideos(folderPath, outputFolder, shape='circle', n_jobs=16):
     """This function will process all videos in a given folder using the above described functions and the csv file
     containing metadata (process.csv)"""
 
-    Parallel(n_jobs=n_jobs)(delayed(processVideo)(os.path.join(folderPath, v), outputFolder)
+    Parallel(n_jobs=n_jobs)(delayed(processVideo)(os.path.join(folderPath, v), outputFolder, shape)
                             for v in os.listdir(folderPath) if v.endswith(".mp4"))
 
 
 if __name__ == "__main__":
     import sys
     processVideos(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+
+
+# # test
+# folder = "C:/Users/serce/Desktop/temp"
+# outputFolder = os.path.join(folder, "outputs")
+# create_trims_df(folder)
+# append_mask_and_crop_ROIs(folder)
+# processVideos(folder, outputFolder, n_jobs=3)
+
