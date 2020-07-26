30431fb631cc3afa533778edf93cf3c1573abcb4
onurserce <onurserce@gmail.com>
2020-07-26 19:05:21 +0200

    minor bug fixes

--- job_scripts/job_process_videos.sh
+++ job_scripts/job_process_videos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p medium
-#SBATCH -t 6:00:00
+#SBATCH -t 12:00:00
 #SBATCH -c 16
 #SBATCH -C scratch
 #SBATCH --mail-type=END
@@ -13,14 +13,14 @@ source activate behaviour-switching
 n_jobs=${1?Error: no n_jobs given}
 
 h_in_dir=$HOME/to_be_processed #CHANGE THIS LINE!
-sc_in_dir=/scratch/onur.serce/
+sc_in_dir=/scratch/onur.serce
 sc_out_dir=$sc_in_dir/outputs
 h_out_dir=$h_in_dir/outputs
 
 mkdir -p $sc_out_dir
-cp h_in_dir/* $scdir
+cp $h_in_dir/* $scdir
 
 python behaviour-switching/process_videos.py $sc_in_dir $sc_out_dir $n_jobs
 
-mkdir h_out_dir
-cp sc_out_dir/* $HOME/h_out_dir
+mkdir $h_out_dir
+cp $sc_out_dir/* $HOME/$h_out_dir
