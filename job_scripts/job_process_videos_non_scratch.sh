1dd0986bc3c7c4680efda539e9c01aa1c61ead4c
onurserce <onurserce@gmail.com>
2020-07-27 10:14:29 +0200

    inital job script

---
+++ job_scripts/job_process_videos_non_scratch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p medium
+#SBATCH -t 12:00:00
+#SBATCH -c 16
+#SBATCH --mail-type=END
+#SBATCH --mail-user=serce@neuro.mpg.de
+#SBATCH -o job_process_videos.sh_%J.out
+
+module purge
+source activate behaviour-switching
+
+n_jobs=${1?Error: no n_jobs given}
+
+wd=$HOME/to_be_processed
+outdir=$HOME/to_be_processed/outputs
+mkdir -p $outdir
+
+python behaviour-switching/process_videos.py $wd $outdir $n_jobs
