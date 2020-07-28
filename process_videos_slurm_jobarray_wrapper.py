e8d918ad0104d5aa43ef5168d246aa439af6ed25
onurserce <onurserce@gmail.com>
2020-07-28 10:47:08 +0200

    added a job script and a python wrapper to process videos using slurm job array

---
+++ process_videos_slurm_jobarray_wrapper.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import pandas as pd
+from process_videos import processVideo
+
+inputfolder = sys.argv[1]
+outputfolder = sys.argv[2]
+array_task_id = int(sys.argv[3])
+
+df = pd.read_csv(os.path.join(inputfolder, "process.csv"), index_col=0)
+videopath = os.path.join(inputfolder, df.index[array_task_id])
+
+processVideo(videopath, outputfolder)
+
+print("Finished: processVideo with arguments", videopath, outputfolder)
