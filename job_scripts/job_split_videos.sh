945b119ffa0d88dc3585f25fdf69f14093bdbfdc
onurserce <onurserce@gmail.com>
2020-08-04 13:37:13 +0200

    code cleanup

--- job_scripts/job_split_videos.sh
+++ job_scripts/job_split_videos.sh
@@ -12,15 +12,9 @@ source activate behaviour-switching
 
 n_jobs=${1?Error: no n_jobs given}
 
-scdir=/scratch/onur.serce/temp_process
-outdir=/scratch/onur.serce/temp_process/outputs
+scdir=/scratch/onur.serce/week2/to_be_split
+outdir=$scdir/outputs
 
 mkdir -p $outdir
-cp $HOME/to_be_split/* $scdir
 
 python behaviour-switching/split_videos.py $scdir $outdir $n_jobs
-
-mkdir $HOME/to_be_split/outputs
-cp outdir/* $HOME/to_be_split/outputs
-
-#rm -rf $scdir
