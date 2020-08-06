05236580998d678e871bf870597b71c3d149ba26
onurserce <onurserce@gmail.com>
2020-08-06 11:26:52 +0200

    implemented dlc_to_anymaze_output function and changed get_anymaze_outputs function to use it within joblib.Parallel.

--- roi_analysis.py
+++ roi_analysis.py
@@ -8,6 +8,7 @@ from drawer import CircleDrawer, PolygonDrawer
 from shapely.geometry import Point
 from shapely.geometry import box
 from shapely.geometry import Polygon
+from joblib import Parallel, delayed
 
 
 def collect_ROIs(folder):
@@ -48,36 +49,38 @@ def draw_ROIs(folder):
                 df.to_csv(os.path.join(folder, 'analysis_rois.csv'))
                 print(os.path.join(folder, 'analysis_rois.csv'), 'updated!')
 
-# analysis_rois = os.path.join(folder, 'analysis_rois.csv')
-# ex_vid = pims.Video(os.path.join(folder, 'ChR2_Day2_processed.mp4'))
-# ex_frame = ex_vid[0]
-def get_anymaze_outputs(folder):
-#todo: modify this function to take single csv or video name as an input to be compatible with slurm parallelization.
+
+def dlc_to_anymaze_output(csv_path, bodypart='center'):
+    """Converts DLC output to anymaze output using analysis_rois.csv file"""
+
+    folder, csv = os.path.split(csv_path)
     analysis_rois = pd.read_csv(os.path.join(folder, 'analysis_rois.csv'), index_col=0)
-    for i in analysis_rois.index:
-        print('Working on ', i)
-        social = literal_eval(analysis_rois.loc[i, '0:circle'])
-        social = Point(social[0]).buffer(social[1])
-        drinking = literal_eval(analysis_rois.loc[i, '1:circle'])
-        drinking = Point(drinking[0]).buffer(drinking[1])
-        marble = literal_eval(analysis_rois.loc[i, '2:rectangle'])
-        marble = box(marble[0], marble[1], marble[0]+marble[2], marble[1]+marble[3])
-        nest = literal_eval(analysis_rois.loc[i, '3:rectangle'])
-        nest = box(nest[0], nest[1], nest[0]+nest[2], nest[1]+nest[3])
-        eating = literal_eval(analysis_rois.loc[i, '4:rectangle'])
-        eating = box(eating[0], eating[1], eating[0]+eating[2], eating[1]+eating[3])
 
-        dlc_df = pd.read_csv(os.path.join(folder, i), header=[1,2])
-        dlc_df = dlc_df['center']   # Taking only the x, y, likelihood of the center position
-        for j in dlc_df.index:
-            mouse_center = Point(dlc_df.loc[j, 'x'], dlc_df.loc[j, 'y'])
-            dlc_df.loc[j, 'social'] = mouse_center.intersects(social)
-            dlc_df.loc[j, 'drinking'] = mouse_center.intersects(drinking)
-            dlc_df.loc[j, 'marble'] = mouse_center.intersects(marble)
-            dlc_df.loc[j, 'nest'] = mouse_center.intersects(nest)
-            dlc_df.loc[j, 'eating'] = mouse_center.intersects(eating)
-        dlc_df.to_csv(os.path.join(folder, i[:-4]+'_ROIs.csv'))     #[:-4] is to remove '.csv' from the filename
+    # Create shapely ROI objects
+    social = literal_eval(analysis_rois.loc[csv, '0:circle'])
+    social = Point(social[0]).buffer(social[1])
+    drinking = literal_eval(analysis_rois.loc[csv, '1:circle'])
+    drinking = Point(drinking[0]).buffer(drinking[1])
+    marble = literal_eval(analysis_rois.loc[csv, '2:rectangle'])
+    marble = box(marble[0], marble[1], marble[0] + marble[2], marble[1] + marble[3])
+    nest = literal_eval(analysis_rois.loc[csv, '3:rectangle'])
+    nest = box(nest[0], nest[1], nest[0] + nest[2], nest[1] + nest[3])
+    eating = literal_eval(analysis_rois.loc[csv, '4:rectangle'])
+    eating = box(eating[0], eating[1], eating[0] + eating[2], eating[1] + eating[3])
 
-# folder = "J:\Alja Podgornik\Multimaze arena\Cohort 1_June 2020\Week 1\\processed\\other videos"
-# get_anymaze_outputs(folder)
+    # Process each DataFrame
+    dlc_df = pd.read_csv(csv_path, header=[1, 2])
+    dlc_df = dlc_df[bodypart]   # Taking only one bodypart
+    for frame in dlc_df.index:
+        mouse_center = Point(dlc_df.loc[frame, 'x'], dlc_df.loc[frame, 'y'])
+        dlc_df.loc[frame, 'social'] = mouse_center.intersects(social)
+        dlc_df.loc[frame, 'drinking'] = mouse_center.intersects(drinking)
+        dlc_df.loc[frame, 'marble'] = mouse_center.intersects(marble)
+        dlc_df.loc[frame, 'nest'] = mouse_center.intersects(nest)
+        dlc_df.loc[frame, 'eating'] = mouse_center.intersects(eating)
+    dlc_df.to_csv(os.path.join(folder, csv[:-4] + '_ROIs.csv'))  # [:-4] is to remove '.csv' from the filename
 
+
+def get_anymaze_outputs(folder, n_jobs=8):
+    Parallel(n_jobs=n_jobs)(delayed(dlc_to_anymaze_output)(os.path.join(folder, csv))
+                            for csv in os.listdir(folder) if (csv.endswith('.csv') and not csv.startswith('analysis')))
