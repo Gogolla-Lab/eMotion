cdeaee533e70d7516b05a5b64896f755154b2e64
onurserce <onurserce@gmail.com>
2020-08-18 23:45:37 +0200

    import_dlc_data changed to import_labeld_dlc_data with substantial improvements

--- utility_functions.py
+++ utility_functions.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
-
+import imageio
+import tqdm
 
 def rename_files(folder, extension):
     """Utility function for renaming files with a certain extension in a folder using renaming.csv file"""
@@ -26,8 +27,8 @@ def rename_files(folder, extension):
             print('Renaming completed!')
 
 
-def import_dlc_data(folder, scorer):
-    """Collects labeled DLC data in a single csv file and creates video from frames"""
+def import_labeled_dlc_data(folder, prefix, scorer, fps=30):
+    """Collects labeled DLC data in a single csv file and creates a video from frames"""
 
     df_accu = pd.DataFrame()
 
@@ -37,7 +38,32 @@ def import_dlc_data(folder, scorer):
             if os.path.exists(os.path.join(subdir, 'CollectedData_'+scorer+'.h5')):
                 data = pd.read_hdf(os.path.join(subdir, 'CollectedData_'+scorer+'.h5'))
                 df_accu = df_accu.append(data)
-    df_accu.to_csv(os.path.join(folder, "accumulated_data_"+scorer+".csv"), index=True)
+
+    scorer = prefix + '_' + scorer  # add prefix to scorer in order to name the files as we wish (don't change above
+    # to not interfere with the hdf readings)
+    count = 0
+    newindex = []
+
+    if not os.path.exists(os.path.join(folder, scorer+'_accumulated')):
+        os.mkdir(os.path.join(folder, scorer+'_accumulated'))
+
+    kwargs = {'macro_block_size': None}     # to prevent imageio_ffmpeg video resizing
+    writer = imageio.get_writer(os.path.join(folder, scorer+'_accumulated.mp4'), fps=fps, **kwargs)
+    for imgpath in tqdm.tqdm(df_accu.index):
+        directory, img = os.path.split(imgpath)
+        directory = os.path.split(directory)[-1]
+        img = imageio.imread(os.path.join(folder, directory, img))
+        writer.append_data(img)
+        newindex.append(os.path.join('labeled-data', scorer+'_accumulated', 'img'+str(count).zfill(5)+'.png'))
+        imageio.imwrite(os.path.join(folder, scorer+'_accumulated', 'img'+str(count).zfill(5)+'.png'), img)
+        count += 1
+    writer.close()
+
+    df_accu.index = newindex
+    df_accu.to_csv(os.path.join(folder, scorer+'_accumulated', "CollectedData" + scorer[len(prefix):] + ".csv"),
+                   index=True)
+    df_accu.to_hdf(os.path.join(folder, scorer+'_accumulated', "CollectedData" + scorer[len(prefix):] + ".h5"),
+                   key="df_with_missing", mode="w", format='table')
 
 
 if __name__ == "__main__":
