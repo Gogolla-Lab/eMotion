8cd226fa7545a7721f4649e10bc9d6b150a1515a
onurserce <onurserce@gmail.com>
2020-08-25 11:47:50 +0200

    added a job dependency spesification for slurm

--- job_scripts/rename_files.sh
+++ job_scripts/rename_files.sh
@@ -4,9 +4,12 @@
 #SBATCH -c 2
 #SBATCH -C scratch
 #SBATCH -o job_rename_files_%J.out
+#SBATCH -d afterok:5217845
 
 module purge
 source activate behaviour-switching
+
+rm -f $HOME/job_array_process_videos*.out
 
 extension=${1?Error: no extension given}
 scdir=/scratch/onur.serce/all_videos/processed
