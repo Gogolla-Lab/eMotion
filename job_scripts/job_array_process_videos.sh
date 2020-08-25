49035d50a339ebf0a1fcdc97a9321f0224909ee0
onurserce <onurserce@gmail.com>
2020-08-25 09:55:27 +0200

    code cleanup for the HPC

--- job_scripts/job_array_process_videos.sh
+++ job_scripts/job_array_process_videos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -a 0-40
+#SBATCH -a 0-154
 #SBATCH -p medium
 #SBATCH --qos=short
-#SBATCH -t 2:00:00
-#SBATCH -c 4
+#SBATCH -t 6:00:00
+#SBATCH -c 1
 #SBATCH -C scratch
 #SBATCH --mail-type=END
 #SBATCH --mail-user=serce@neuro.mpg.de
@@ -12,7 +12,7 @@
 module purge
 source activate behaviour-switching
 
-sc_in_dir=/scratch/onur.serce
-sc_out_dir=$sc_in_dir/outputs
+sc_in_dir=/scratch/onur.serce/all_videos
+sc_out_dir=$sc_in_dir/processed
 
 python behaviour-switching/process_videos_slurm_jobarray_wrapper.py $sc_in_dir $sc_out_dir $SLURM_ARRAY_TASK_ID
