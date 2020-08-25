fe390bb9b79a1210fec0fcf1af43beb63f180cf8
onurserce <onurserce@gmail.com>
2020-08-25 22:25:21 +0200

    added a delay between 1 to 180 seconds before editing the config to avoid writing at the same time by parallel jobs

--- dlc_analyse_videos_jobarray.py
+++ dlc_analyse_videos_jobarray.py
@@ -1,4 +1,6 @@
 import os
+from time import sleep
+from random import uniform
 
 os.environ["DLClight"] = "True"
 import sys
@@ -15,10 +17,10 @@ config_path = "/usr/users/onur.serce/dlc_real-alja_onur-2020-04-06/config.yaml"
 videos = [os.path.join(videofolder, vid) for vid in os.listdir(videofolder) if
           (vid.endswith('.mp4') or vid.endswith('.MP4'))]
 
+sleep(uniform(1, 180))
 edits = {'snapshotindex': snapshotindex}
 dlc.auxiliaryfunctions.edit_config(config_path, edits)
-for item in edits.items():
-    print(item)
+
 
 dlc.analyze_videos(config=config_path, videos=videos[index:index + 1], videotype='.mp4', shuffle=shuffleindex,
                    trainingsetindex=0, gputouse=gputouse, save_as_csv=True, TFGPUinference=True)
