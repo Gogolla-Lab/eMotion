d903e9aa6cc59a427c9f7f318d1ed409a5a49d0d
onurserce <onurserce@gmail.com>
2020-08-28 21:44:17 +0200

    changed to job_array_roi_analysis.sh

--- job_scripts/job_array_roi_analysis.sh
+++ job_scripts/job_array_roi_analysis.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH -a 0-155
+#SBATCH -a 0-153
 #SBATCH -p medium
 #SBATCH --qos=short
 #SBATCH -t 30:00
 #SBATCH -c 1
 #SBATCH -C scratch
-#SBATCH -o job_array_get_anymaze_output_%A_%a.out
+#SBATCH -o job_array_roi_analysis_%A_%a.out
 #SBATCH --mail-type=END
 #SBATCH --mail-user=serce@neuro.mpg.de
 
 module purge
 source activate behaviour-switching
 
-folder=${1?Error: Please provide the path to the folder containing analysis.csv and DLC outputs.}
+folder=${1?Error: Please provide the path to the folder containing analysis.csv and DLC .h5 outputs.}
 
 python behaviour-switching/roi_analysis.py "$folder" "$SLURM_ARRAY_TASK_ID"
 
