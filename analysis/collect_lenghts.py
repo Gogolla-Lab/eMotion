58a907da0bd3025da1f199d9afb845dd9e33cb1b
onurserce <onurserce@gmail.com>
2020-09-01 11:53:00 +0200

    collect_lenghts function improved

--- analysis/collect_lenghts.py
+++ analysis/collect_lenghts.py
@@ -1,21 +1,31 @@
 import os
-import numpy as np
 import pandas as pd
 import pims
 from drawer import LineDrawer
+from math import sqrt
 
-def collect_lenghts(folder):
 
+def collect_lengths(folder):
     videos = [mp4 for mp4 in os.listdir(folder) if (mp4.endswith(".mp4"))]
 
     if not os.path.exists(os.path.join(folder, 'lenghts.csv')):
-        df = pd.DataFrame(index=videos, columns=['line', 'known_length_px', 'known_length_cm', 'frame_shape'])
-        df.to_csv(os.path.join(folder, 'lenghts.csv'))
+        df = pd.DataFrame(index=videos, columns=['line', 'frame_shape', 'known_length_px', 'known_length_cm'])
+        df.to_csv(os.path.join(folder, 'lengths.csv'))
 
-    df = pd.read_csv(os.path.join(folder, 'lenghts.csv'), index_col=0)
+    df = pd.read_csv(os.path.join(folder, 'lengths.csv'), index_col=0)
     for vid in df.index:
         if pd.isnull(df.loc[vid, 'line']):
             video = pims.Video(os.path.join(folder, vid))
-            ld = LineDrawer("Draw a LINE, RIGHT click when you're ready!")
-            line = (ld.run(thickness=1))
-            df.loc[vid, 'line'] = line
+            frame_shape = video.frame_shape
+            df.loc[vid, 'frame_shape'] = str(frame_shape)
+            ld = LineDrawer("Draw a LINE, RIGHT click when you're ready!", video[0])
+            line = ld.run(thickness=1)
+            df.loc[vid, 'line'] = str(line)
+            known_length_px = sqrt((line[1][1] - line[0][1])**2 + (line[1][0] - line[0][0])**2)    # Euclidean distance
+            df.loc[vid, 'known_length_px'] = known_length_px
+
+            df.to_csv(os.path.join(folder, 'lengths.csv'))
+    df.to_csv(os.path.join(folder, 'lengths.csv'))
+
+folder = r"J:\Alja Podgornik\Multimaze arena\Cohort 1_June 2020\Week 1\temp"
+collect_lengths(folder)
