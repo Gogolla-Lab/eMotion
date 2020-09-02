066485fd515ae52d2aaf626cb85fb23b727dc1c4
onurserce <onurserce@gmail.com>
2020-09-02 20:00:19 +0200

    code cleanup

--- dlc_create_training_comparison.py
+++ dlc_create_training_comparison.py
@@ -9,6 +9,6 @@ print("'config_path' is:", config_path)
 print("'dlc.__version__' is:'", dlc.__version__)
 print("\n")
 
-dlc.create_training_model_comparison(config_path, num_shuffles=3, net_types=['resnet_101', 'resnet_152'], augmenter_types=['imgaug', 'tensorpack', 'deterministic', 'default'], userfeedback=False, windows2linux=True)
+dlc.create_training_model_comparison(config_path, num_shuffles=10, net_types=['resnet_152'], augmenter_types=['imgaug'], userfeedback=False, windows2linux=True)
 
 print('\n', 'create_training_model_comparison completed!')
