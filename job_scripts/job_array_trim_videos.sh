be453fe6df396d0a9b673edf53609f6223b73ed6
onurserce <onurserce@gmail.com>
2020-11-05 13:30:50 +0100

    init

---
+++ job_scripts/job_array_trim_videos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -a 0-39
+#SBATCH -p medium
+#SBATCH --qos=short
+#SBATCH -t 1:58:58
+#SBATCH -c 4
+#SBATCH --mail-type=END
+#SBATCH --mail-user=serce@neuro.mpg.de
+#SBATCH -o job_array_trim_videos_%A_%a.out
+
+module purge
+source activate behaviour-switching
+
+wd=$HOME/to_be_trimmed
+outdir=$wd/outputs
+mkdir -p "$outdir"
+
+python behaviour-switching/trim_videos_slurm_jobarray_wrapper.py "$wd" "$outdir" "$SLURM_ARRAY_TASK_ID"
---
+++ trim_videos_slurm_jobarray_wrapper.py
@@ -0,0 +1,15 @@
+import sys
+import os
+import pandas as pd
+from process_videos import trimVideo
+
+inputfolder = sys.argv[1]
+outputfolder = sys.argv[2]
+array_task_id = int(sys.argv[3])
+
+df = pd.read_csv(os.path.join(inputfolder, "trims.csv"), index_col=0)
+videopath = os.path.join(inputfolder, df.index[array_task_id])
+
+trimVideo(videopath, outputfolder)
+
+print("Finished: trimVideo with arguments", videopath, outputfolder)
