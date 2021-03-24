1224a62d52b6bd4b6f656eadf116355c18721344
onurserce <onurserce@gmail.com>
2021-03-24 14:57:05 +0100

    cleanup

--- FiberPhotometry/color_code_behavior.py
+++ FiberPhotometry/color_code_behavior.py
@@ -4,7 +4,11 @@ import pandas as pd
 import numpy as np
 from datetime import timedelta, datetime
 from os.path import join
+from os import chdir
+
 # %matplotlib qt
+
+chdir(r"J:\Alja Podgornik\FP_Alja")
 
 
 def find_nearest(array, value):
@@ -21,7 +25,8 @@ def get_mpl_datetime(time):
     dt = timedelta(minutes=int(split_time[0]), seconds=int(split_time[1]))
     return md.date2num(zero + dt)
 
-# Make sure that all of the times in the csv are in the "Text", rather than a "Number" format 
+
+# Make sure that all of the times in the csv are in the "Text", rather than a "Number" format
 # All the times must be in a two decimal format; always 15.50, never 15.5
 def mpl_datetime_from_seconds(time: float):
     """ Takes either an integer or an array and converts it to mpl datetime format"""
@@ -52,7 +57,6 @@ def plot_dff(time, f_trace, ax=None):
 
 
 def plot_episodes(time: np.array, f_trace: np.array, labels_df: pd.DataFrame, behavior="All"):
-
     # For a list of all MPL colors: https://matplotlib.org/stable/gallery/color/named_colors.html
     behavior_color = {'Eating': 'cyan',
                       'Grooming': 'goldenrod',
@@ -63,18 +67,16 @@ def plot_episodes(time: np.array, f_trace: np.array, labels_df: pd.DataFrame, be
                       'Social Interaction': 'deeppink',
                       'Ear Scratch': 'firebrick',
                       'Switch': 'sienna',
-                      'Idle':'silver',
+                      'Idle': 'silver',
                       'Nibbling Floor': 'thistle',
                       'Nibbling Tape': 'aquamarine',
                       'Shock': 'red',
                       }
-                      
 
     zone_color = {'Eating Zone': 'b',
                   'Marble Zone': 'g',
                   'Nesting Zone': 'gray',
                   }
-
 
     # Searches the dataframe for each behavior type, zone type, or other action
     behaviors = [" ".join(col.split(" ")[0:-1]) for col in labels_df.columns if "Start" in col.split(" ")[-1]]
@@ -84,7 +86,7 @@ def plot_episodes(time: np.array, f_trace: np.array, labels_df: pd.DataFrame, be
 
     # Get the dF/F plot
     plot_dff(time, f_trace, ax=ax1)
-    #ax1.xaxis.label.set_visible(False)
+    # ax1.xaxis.label.set_visible(False)
 
     # Convert seconds into HH:MM:SS format
     time_HMS = mpl_datetime_from_seconds(time)
@@ -101,9 +103,8 @@ def plot_episodes(time: np.array, f_trace: np.array, labels_df: pd.DataFrame, be
         start_end_behavior = labels_df[[behav + " Start", behav + " End"]].dropna().to_numpy()
 
         if len(start_end_behavior) > 0:
-        
+
             for episode in start_end_behavior:
-    
                 start_idx, start_time = find_nearest(time_HMS, get_mpl_datetime(episode[0]))
                 end_idx, end_time = find_nearest(time_HMS, get_mpl_datetime(episode[1]))
                 b = ax1.axvspan(start_time, end_time, facecolor=behavior_color[behav], alpha=0.5)
@@ -111,47 +112,42 @@ def plot_episodes(time: np.array, f_trace: np.array, labels_df: pd.DataFrame, be
             behav_vspan.append([b, behav])
 
     behav_vspan = np.array(behav_vspan)
-    #ax1.axhline(2, ls='--', c='gray')
+    # ax1.axhline(2, ls='--', c='gray')
     ax1.axhline(0, ls='--', c='gray')
-    #ax1.axhline(-2, ls='--', c='gray')
+    # ax1.axhline(-2, ls='--', c='gray')
     ax1.legend(behav_vspan[:, 0], behav_vspan[:, 1], loc="upper right")
 
-    #TODO scratching
+    # TODO scratching
     # for other in others:
     #     start_instance = labels_df[[other]].dropna().to_numpy()
     #     instance_idx, instance_time = find_nearest(time_HMS, get_mpl_datetime(start_instance))
 
-
-
-
-
     # Add a subplot containing the times in a certain zone
     plot_dff(time, f_trace, ax=ax2)
-    
+
     zone_vspan = []
     for zone in zones:
 
         start_end_zone = labels_df[[" ".join([zone, "In"]), " ".join([zone, "Out"])]].dropna().to_numpy()
-        
+
         if len(start_end_zone) > 0:
             bool_array = np.zeros(len(time_HMS))
-            
+
             for episode in start_end_zone:
-    
                 start_idx, start_time = find_nearest(time_HMS, get_mpl_datetime(episode[0]))
                 end_idx, end_time = find_nearest(time_HMS, get_mpl_datetime(episode[1]))
                 bool_array[start_idx:end_idx + 1] = 1
                 z = ax2.axvspan(start_time, end_time, facecolor=zone_color[zone], alpha=0.2)
-    
+
             zone_vspan.append([z, zone])
         # ax2.plot(time_HMS, bool_array, c=zone_color[zone])
 
     # ax2.set_ylim([0, 1])
-    #ax2.axhline(2, ls='--', c='gray')
+    # ax2.axhline(2, ls='--', c='gray')
     ax2.axhline(0, ls='--', c='gray')
-    #ax2.axhline(-2, ls='--', c='gray')
+    # ax2.axhline(-2, ls='--', c='gray')
     ax2.set_xlabel('Time')
-    
+
     zone_vspan = np.array(zone_vspan)
     ax2.legend(zone_vspan[:, 0], zone_vspan[:, 1], loc="upper right")
 
@@ -164,17 +160,15 @@ if __name__ == "__main__":
     mouse_ID = 4
     day = 1
 
-
     save_directory = r"J:\Alja Podgornik\FP_Alja\plots"
     behavior_dir = r"J:\Alja Podgornik\FP_Alja\Multimaze scoring"
     dff_dir = r"J:\Alja Podgornik\FP_Alja\FP_processed data"
 
-    
-    behavior_filename = r"ID{}_Day{}.xlsx".format(mouse_ID, day)    # change path
+    behavior_filename = r"ID{}_Day{}.xlsx".format(mouse_ID, day)  # change path
     x = pd.ExcelFile(join(behavior_dir, behavior_filename), engine='openpyxl')
     behavior_labels = pd.read_excel(x, header=0, dtype=object, engine='openpyxl')
 
-    dff_filename = r"{}.{}.npy".format(mouse_ID, day)          # change path
+    dff_filename = r"{}.{}.npy".format(mouse_ID, day)  # change path
     data = np.load(join(dff_dir, dff_filename), allow_pickle=True)
     data = data.item()
 
@@ -187,6 +181,6 @@ if __name__ == "__main__":
 
     fig = plot_episodes(fp_times, dffzscore, behavior_labels)
     plt.suptitle(" ".join((str(Ani_ID), 'Z-Score DFF', 'behavior segmentation')))
-    plt.savefig(join(save_directory, " ".join((str(Ani_ID), 'Z-Score DFF', 'behavior segmentation')) + ".png"))            # change path for
+    plt.savefig(join(save_directory,
+                     " ".join((str(Ani_ID), 'Z-Score DFF', 'behavior segmentation')) + ".png"))  # change path for
     plt.show()
-
--- FiberPhotometry/mean_behavior_episodes.py
+++ FiberPhotometry/mean_behavior_episodes.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import os
 from os.path import join
 from datetime import timedelta, datetime
 import matplotlib.dates as md
 from scipy.signal import medfilt
 from scipy.stats import sem
+from os import chdir
 
-
+chdir(r"J:\Alja Podgornik\FP_Alja")
 
 def find_nearest(array, value):
     array = np.asarray(array)
@@ -17,11 +17,10 @@ def find_nearest(array, value):
 
 
 def get_sec_from_min_sec(time: float):
-     split_time = str(time).split('.')
-     minutes = int(split_time[0])
-     seconds = int(split_time[1])
-     return minutes*60 + seconds
-
+    split_time = str(time).split('.')
+    minutes = int(split_time[0])
+    seconds = int(split_time[1])
+    return minutes * 60 + seconds
 
 
 def get_mpl_datetime(time: float, seconds_adjustment=0):
@@ -44,13 +43,12 @@ def mpl_datetime_from_seconds(time):
 
 
 def read_summary_file(file_path):
-
-    summ_file = pd.ExcelFile(file_path)
+    summ_file = pd.ExcelFile(file_path, engine='openpyxl')
     sheets = list(summ_file.sheet_names)
     sheet_dfs = []
 
     for sheet in sheets:
-        df = pd.read_excel(summ_file, header=0, sheet_name=sheet, dtype=object)
+        df = pd.read_excel(summ_file, header=0, sheet_name=sheet, dtype=object, engine='openpyxl')
         day = int(sheet[-1])
         df['Day'] = [day for i in range(len(df))]
         sheet_dfs.append(df)
@@ -64,11 +62,11 @@ def load_from_excel_summary(exp_metadata):
 
     behavior_label_path = exp_metadata['Behavior Labelling']
     behavior_filename = r"ID{}_Day{}.xlsx".format(*ani_id.split('.'))
-    x = pd.ExcelFile(join(behavior_label_path, behavior_filename))
-    behavior_labels = pd.read_excel(x, header=0, dtype=object)
+    x = pd.ExcelFile(join(behavior_label_path, behavior_filename), engine='openpyxl')
+    behavior_labels = pd.read_excel(x, header=0, dtype=object, engine='openpyxl')
 
     preprocessed_data_path = exp_metadata['Preprocessed Data']
-    dff_filename = r"{}.npy".format(ani_id)               # change path
+    dff_filename = r"{}.npy".format(ani_id)  # change path
     data = np.load(join(preprocessed_data_path, dff_filename), allow_pickle=True)
     data = data.item()
 
@@ -76,32 +74,31 @@ def load_from_excel_summary(exp_metadata):
 
 
 def find_episodes(time, f_trace, labels, key, period=(-5, 5), dwell_filter=0):
-     exp_episodes = []
-     
-     # Convert seconds into HH:MM:SS format
-     # time_HMS = mpl_datetime_from_seconds(time)
-     
-     if "Zone" in key:
-         start_end = labels[[" ".join([key, "In"]), " ".join([key, "Out"])]].dropna().to_numpy()
-     else:
-         start_end = labels[[" ".join([key, "Start"]), " ".join([key, "End"])]].dropna().to_numpy()
-    
-     if len(start_end) > 0:
-         vfunc = np.vectorize(get_sec_from_min_sec)
-         start_end = vfunc(start_end)
-         
-         for episode in start_end:
-             
-             dwell_time = episode[1] - episode[0]
-             
-             if dwell_time <= dwell_filter:
-                 
-                 start_idx, start_time = find_nearest(time, episode[0] + period[0])
-                 end_idx, end_time = find_nearest(time, episode[0] + period[1])
-                 
-                 exp_episodes.append([time[start_idx:end_idx], f_trace[start_idx:end_idx]])
+    exp_episodes = []
 
-     return exp_episodes
+    # Convert seconds into HH:MM:SS format
+    # time_HMS = mpl_datetime_from_seconds(time)
+
+    if "Zone" in key:
+        start_end = labels[[" ".join([key, "In"]), " ".join([key, "Out"])]].dropna().to_numpy()
+    else:
+        start_end = labels[[" ".join([key, "Start"]), " ".join([key, "End"])]].dropna().to_numpy()
+
+    if len(start_end) > 0:
+        vfunc = np.vectorize(get_sec_from_min_sec)
+        start_end = vfunc(start_end)
+
+        for episode in start_end:
+
+            dwell_time = episode[1] - episode[0]
+
+            if dwell_time <= dwell_filter:
+                start_idx, start_time = find_nearest(time, episode[0] + period[0])
+                end_idx, end_time = find_nearest(time, episode[0] + period[1])
+
+                exp_episodes.append([time[start_idx:end_idx], f_trace[start_idx:end_idx]])
+
+    return exp_episodes
 
 
 def flatten_list(list_of_lists):
@@ -112,10 +109,10 @@ def list_lists_to_array(list_of_lists):
     max_length = max([len(l) for l in list_of_lists])
     new_array = np.empty((len(list_of_lists), max_length))
     new_array[:] = np.NaN
-    
+
     for row, l in enumerate(list_of_lists):
         new_array[row, :len(l)] = l
-    
+
     return new_array
 
 
@@ -124,11 +121,12 @@ def get_mean_episode(episodes):
     f_traces = [e[1] for e in episodes]
 
     trace_array = list_lists_to_array(f_traces)
-    
+
     mean_trace = np.nanmean(trace_array, axis=0)
     std_trace = np.nanstd(trace_array, axis=0)
-    
+
     return trace_array, mean_trace, std_trace
+
 
 # norm.window here is a default; if you don't pass the parameter in the code lower down it will resort to -5
 def remove_baseline(time, traces, norm_window=-5):
@@ -141,15 +139,15 @@ def remove_baseline(time, traces, norm_window=-5):
 
 if __name__ == "__main__":
 
-    summary_file_path = r'Multimaze sheet summary.xlsx'    # Set this to wherever it is
-    save_directory = r"J:\Alja Podgornik\FP_Alja\plots"    # Set this to wherever you want
+    summary_file_path = r'Multimaze sheet summary.xlsx'  # Set this to wherever it is
+    save_directory = r"J:\Alja Podgornik\FP_Alja\plots"  # Set this to wherever you want
 
     # Read the summary file as a pandas dataframe
     all_exps = read_summary_file(summary_file_path)
-    
+
     # remove certain days
     exps_to_run = all_exps
-    #exps_to_run = all_exps.loc[all_exps["Day"] == 3]
+    # exps_to_run = all_exps.loc[all_exps["Day"] == 3]
 
     # Which behavior do you want to look at
     key_to_plot = 'Eating Zone'
@@ -163,52 +161,50 @@ if __name__ == "__main__":
         try:
             # load the raw data from 1 rec at a time
             data, labels = load_from_excel_summary(row)
-    
+
             Ani_ID = data['ani_id']
             time = data['ts']
             auto = data['auto']
             gcamp = data['gcamp']
             dff = data['dff']
             dffzscore = data['zscore']
-            
+
             dffzscore = medfilt(dffzscore, kernel_size=51)
-            
+
             exp_episodes = find_episodes(time, dffzscore, labels, key_to_plot, period=period, dwell_filter=dfilt)
             all_episodes.append(exp_episodes)
-        
+
         except FileNotFoundError as error:
             print(str(error))
-    
+
     all_episodes = flatten_list(all_episodes)
     all_episodes = list(filter(None, all_episodes))
     f_traces = [e[1] for e in all_episodes]
     trace_array = list_lists_to_array(f_traces)
-    
+
     num_episodes = trace_array.shape[0]
     print("Number of {} trials = {}".format(key_to_plot, num_episodes))
-    
+
     t = np.linspace(*period, trace_array.shape[-1])
     trace_array = remove_baseline(t, trace_array, norm_window=-5)
-    
+
     mean_trace = np.nanmean(trace_array, axis=0)
     sem_trace = sem(trace_array, axis=0, nan_policy='omit')
-    
-    fig = plt.figure(figsize=(10,10))
-    #for trace in trace_array:
-         #plt.plot(t, trace)
-        
+
+    fig = plt.figure(figsize=(10, 10))
+    # for trace in trace_array:
+    # plt.plot(t, trace)
+
     plt.fill_between(t, mean_trace - sem_trace, mean_trace + sem_trace, alpha=0.2)
     plt.plot(t, mean_trace, c='k', linewidth=2)
-    #plt.ylim([-0.25, 1.5])
+    # plt.ylim([-0.25, 1.5])
     plt.axvline(0, color="orangered")
     plt.text(-4.5, 0.3, "n = " + str(num_episodes), fontsize='large')
-        
+
     plt.xlabel('Time from Behavior Start (s)')
     plt.ylabel('$\Delta$FF Z-score minus')
     plt.title('Mean trace for {}'.format(key_to_plot))
-    
+
     plt_name = "mean_{}_dff_zscore.png".format(key_to_plot.lower())
     plt.savefig(join(save_directory, plt_name))
     plt.show()
-    
-
