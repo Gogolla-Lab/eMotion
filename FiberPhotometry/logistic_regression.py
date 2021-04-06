20b916458d9cffd33ceafa8ed64b5b0618b23350
onurserce <onurserce@gmail.com>
2021-04-06 10:40:52 +0200

    refactor

--- FiberPhotometry/logistic_regression.py
+++ FiberPhotometry/logistic_regression.py
@@ -1,8 +1,5 @@
-import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sb
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
