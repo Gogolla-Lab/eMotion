7a23e77b771f32988b65b00c32fbce46654eac52
onurserce <onurserce@gmail.com>
2021-07-05 15:32:47 +0200

    updated cuda and cudnn modules for HPC compatibility

--- job_scripts/job_array_evaluate_network.sh
+++ job_scripts/job_array_evaluate_network.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 #SBATCH -a 0-9
 #SBATCH -p gpu
-#SBATCH -x dge007
 #SBATCH --qos=short
 #SBATCH -t 1:30:00
 #SBATCH -G gtx1080:1
-#SBATCH -c 2
 #SBATCH --mail-type=END
 #SBATCH --mail-user=serce@neuro.mpg.de
 #SBATCH -o evaluate_network_%A_%a.out
 
 module purge
-module load cuda10.0/toolkit/10.0.130
-module load cuda10.0/blas/10.0.130
-module load cudnn/10.0v7.6.3
-source activate behaviour-switching
+module load cuda/11.1.0
+module load cudnn/7.6.5.32-10.2-linux-x64
+
+source "$HOME"/.bashrc
+source activate DLC-GPU
 
 gputouse=$CUDA_VISIBLE_DEVICES
 
 python behaviour-switching/worker_scripts/dlc_evaluate_network.py "$SLURM_ARRAY_TASK_ID" "$gputouse"
-
 echo "dlc_evaluate_network.py $SLURM_ARRAY_TASK_ID $gputouse completed!"
 
 #Manually edit:
