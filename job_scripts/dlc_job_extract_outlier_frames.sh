d1c4c8a80d1ab653bc12856cdaf4f7b7ae530ec3
onurserce <onurserce@gmail.com>
2021-07-22 16:53:53 +0200

    updated scripts for slurm job array & dlc2.2rc2 compatibility

--- job_scripts/dlc_job_extract_outlier_frames.sh
+++ job_scripts/dlc_job_extract_outlier_frames.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#SBATCH -p gpu
-#SBATCH -t 12:00:00
-#SBATCH -G gtx1080:1
+#SBATCH -a 0:63
+#SBATCH -t 2:00:00
+#SBATCH --qos=short
+#SBATCH -c 2
+#SBATCH --mem=80G
 #SBATCH --mail-type=END
 #SBATCH --mail-user=serce@neuro.mpg.de
-#SBATCH -o dlc_job_extract_outlier_frames_%J.out
+#SBATCH -o dlc_job_extract_outlier_frames_%A_%a.out
 
 
 module purge
-module load cuda/11.1.0
-module load cudnn/8.0.4.30-11.1-linux-x64
+#module load cuda/11.1.0
+#module load cudnn/8.0.4.30-11.1-linux-x64
 
 source "$HOME"/.bashrc
 source activate DLC-GPU
@@ -19,10 +21,7 @@ snapshotindex=${2?Error: no snapshotindex given}
 outlieralgorithm=${3?Error: no outlier algorithm given}
 epsilon=${4?Error: no epsilon given}
 extractionalgorithm=${5?Error: no extraction algorithm given}
-nframes=${6?Error: no number of frames to extract given}
 
-nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits
+python eMotion/worker_scripts/dlc_extract_outlier_frames.py "$shuffleindex" "$snapshotindex" "$outlieralgorithm" "$epsilon" "$extractionalgorithm" "$SLURM_ARRAY_TASK_ID"
 
-python eMotion/worker_scripts/dlc_extract_outlier_frames.py "$shuffleindex" "$snapshotindex" "$outlieralgorithm" "$epsilon" "$extractionalgorithm" "$nframes"
-
-echo "dlc_extract_outlier_frames.py $shuffleindex $snapshotindex $outlieralgorithm $epsilon $extractionalgorithm $nframes is completed!"
+echo "dlc_extract_outlier_frames.py $shuffleindex $snapshotindex $outlieralgorithm $epsilon $extractionalgorithm $SLURM_ARRAY_TASK_ID is completed!"
--- worker_scripts/dlc_extract_outlier_frames.py
+++ worker_scripts/dlc_extract_outlier_frames.py
@@ -1,5 +1,6 @@
 import os
-
+from time import sleep
+from random import uniform
 os.environ["DLClight"] = "True"
 import sys
 
@@ -8,17 +9,17 @@ snapshotindex = int(sys.argv[2])
 outlieralgorithm = sys.argv[3]
 epsilon = float(sys.argv[4])
 extractionalgorithm = sys.argv[5]
-nframes = int(sys.argv[6])
+videoindex = int(sys.argv[6])
 
 import deeplabcut as dlc
 
-config_path = "/usr/users/onur.serce/dlc_real-alja_onur-2020-04-06/config.yaml"
-videos_path = "/usr/users/onur.serce/dlc_real-alja_onur-2020-04-06/videos/"
+config_path = "/usr/users/onur.serce/emotipose-Stoyo-2021-05-10/config.yaml"
+videos_path = "/usr/users/onur.serce/isopreteranol"
 
 videos_path_list = []
 for video in os.listdir(videos_path):
     if video.endswith(".mp4") or video.endswith(".MP4"):
-        videos_path_list.append(videos_path + video)
+        videos_path_list.append(os.path.join(videos_path, video))
 
 # This is to resume the training from where it left off, don't forget to remove this!
 # videos_path_list = videos_path_list[10:]
@@ -33,6 +34,8 @@ print("str(sys.argv):", str(sys.argv), "\n")
 print("\n")
 print("\n")
 
+sleep(uniform(0, 240))
+
 edits = {'snapshotindex': snapshotindex, 'numframestopick': nframes}
 print('\nediting the config file... ')
 for item in edits.items():
@@ -40,9 +43,29 @@ for item in edits.items():
 dlc.auxiliaryfunctions.edit_config(config_path, edits)
 print('edit completed!')
 
-dlc.extract_outlier_frames(config=config_path, videos=videos_path_list, videotype='.mp4', shuffle=shuffleindex,
-                           trainingsetindex=0, outlieralgorithm=outlieralgorithm, epsilon=epsilon, automatic='True',
-                           extractionalgorithm=extractionalgorithm)
+dlc.extract_outlier_frames(
+    config=config_path,
+    videos=videos_path_list[videoindex:videoindex+1],
+    videotype='.mp4',
+    shuffle=shuffleindex,
+    trainingsetindex=0,
+    outlieralgorithm=outlieralgorithm,
+    comparisonbodyparts='all',
+    epsilon=epsilon,
+    p_bound=0.01,
+    ARdegree=3,
+    MAdegree=1,
+    alpha=0.01,
+    extractionalgorithm=extractionalgorithm,
+    automatic=True,
+    cluster_resizewidth=30,
+    cluster_color=False,
+    opencv=True,
+    savelabeled=False,
+    destfolder=None,
+    modelprefix='',
+    track_method='',
+)
 
 print("dlc_extract_outlier_frames with the call", str(sys.argv), "is done!")
 
