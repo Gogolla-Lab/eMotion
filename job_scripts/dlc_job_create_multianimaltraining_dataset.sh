9bab6a101690e3a742db3244d6a4e2a54e60a47e
onurserce <onurserce@gmail.com>
2021-07-30 23:16:51 +0200

    added new slurm job submission scripts

---
+++ job_scripts/dlc_job_create_multianimaltraining_dataset.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --qos=short
+#SBATCH -t 2:00:00
+#SBATCH -c 4
+#SBATCH --mail-type=END
+#SBATCH --mail-user=serce@neuro.mpg.de
+#SBATCH -o dlc_create_multianimaltraining_dataset_%J.out
+
+module purge
+
+source "$HOME"/.bashrc
+source activate DLC-GPU
+
+config=${1?Error: no config.yaml path given}
+num_shuffles=${1?Error: no num_shuffles given}
+
+python eMotion/worker_scripts/dlc_create_multianimaltraining_dataset.py "$config" "$num_shuffles"
+
+echo "Job Completed!"
---
+++ worker_scripts/dlc_create_multianimaltraining_dataset.py
@@ -0,0 +1,23 @@
+import os
+os.environ["DLClight"]="True"
+import deeplabcut as dlc
+import sys
+
+config_path = sys.argv[1]
+num_shuffles = int(sys.argv[2])
+
+print("'config_path' is:", config_path)
+print("'dlc.__version__' is:'", dlc.__version__)
+print("\n")
+
+dlc.create_multianimaltraining_dataset(
+    config=config,
+    num_shuffles=num_shuffles,
+    Shuffles=None,
+    windows2linux=False,
+    net_type='resnet_152',
+    numdigits=2,
+    paf_graph=None,
+)
+
+print('\n', 'dlc_create_multianimaltraining_dataset.py completed!')
